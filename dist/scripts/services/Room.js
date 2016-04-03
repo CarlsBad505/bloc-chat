5be9715729c1b09655814740ff9014df3bdd8b8f
Carl Truong <Carl@tabrific.com>
2016-04-03 17:16:44 -0600

    modify styling, add nodes to child branch in DB

--- dist/scripts/services/Room.js
+++ dist/scripts/services/Room.js
@@ -1,14 +1,23 @@
 (function() {
 	function Room($firebaseArray) {
-		var firebaseRef = new Firebase('https://blistering-fire-7074.firebaseio.com')
 		
-		var rooms = $firebaseArray(firebaseRef.child('rooms'));
-		
-		
-		
-		return {
-			all: rooms
+		var rooms = {
+			getRooms: getRooms
 		};
+		
+		return rooms;
+		
+		function getRooms() {
+			var firebaseRef = new Firebase('https://blistering-fire-7074.firebaseio.com');
+			var roomRef = $firebaseArray(firebaseRef.child('rooms'));
+// 			Only Need to add these Default Rooms in Once, upon initiation of DB.
+//			roomRef.$add({ name: "Room 1" });
+//			roomRef.$add({ name: "Room 2" });
+//			roomRef.$add({ name: "Room 3" });
+			return {
+				all: $firebaseArray(firebaseRef.child('rooms'))
+			}
+		}
 	}
 	
 	angular
