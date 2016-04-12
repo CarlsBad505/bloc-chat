d40936f908dcf73f7d9bab45d61b93dc33a72fe7
Carl Truong <Carl@tabrific.com>
2016-04-11 22:23:25 -0600

    create Message.js factory service, users now able to submit username and send messages

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,11 +1,16 @@
 (function() {
-	function HomeCtrl(Room, $uibModal){
+	function HomeCtrl(Room, Message, $uibModal){
 		var vm = this;
 		vm.chatRooms = Room.getRooms().all;
         vm.selectRoom = function(room) {
 			vm.selectedRoom = room;
-			vm.messages = Room.getMessages(this.selectedRoom.$id);
+			vm.messages = Room.getMessages(vm.selectedRoom.$id);
 		}
+		
+		vm.sendMessage = function() {
+			Message.send(vm.newMessage, vm.selectedRoom.$id);
+			vm.newMessage = '';
+		} 
 		
 		vm.openModal = function() {
 			var modalInstance = $uibModal.open({
@@ -31,5 +36,5 @@
 	
 	angular
 		.module('blocChat')
-		.controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+		.controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
 })();
